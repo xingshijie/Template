4b14dc3fda06f8fb0d320429aed7359eb23e26e9
word-xing <860189975@qq.com>
2016-05-11 11:34:06 +0800

    ButterKnife最佳实践，设置了只要有butterknife.BindView，或butterknife.OnClick注释，未使用的方法或属性就不会有警告

--- app/src/main/java/com/xingshijie/template/MainActivity.java
+++ app/src/main/java/com/xingshijie/template/MainActivity.java
@@ -1,13 +1,43 @@
 package com.xingshijie.template;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static butterknife.ButterKnife.findById;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_butter_knife)
+    Button mBtnButterKnife;
+    @BindView(R.id.btn_retrofit)
+    Button mBtnRetrofit;
+
+    Button mBtnGlide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mBtnGlide = findById(this, R.id.btn_glide);
+    }
+
+    @OnClick({R.id.btn_butter_knife, R.id.btn_retrofit, R.id.btn_glide})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_butter_knife:
+                break;
+            case R.id.btn_retrofit:
+                break;
+            case R.id.btn_glide:
+                break;
+        }
     }
 }
